5506280c5d63ebf473b2c68527e7d90e691c8cf5
Kyle Shannon <kyle@pobox.com>
2015-12-14 15:59:55 -0700

    delineate multiple returns

--- src/pw.cpp
+++ src/pw.cpp
@@ -1,3 +1,8 @@
+/*
+** Copyright (c) 2015 Boise Center Aerospace Laboratory
+**
+** Author: Kyle Shannon <kyle at pobox dot com>
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,9 +12,17 @@
 
 #define INFILE "/home/kyle/src/bsu/lidar/data/riegl_example4.pls"
 
+/*
+** This program opens and reads a pulse waves file, and outputs pertinent
+** information for each in/out waveform.  The output should be consumable by
+** matlab.
+*/
+
 int main(int argc, char *argv[]) {
     PULSEreadOpener pOpener;
     PULSEreader *pReader;
+    WAVESsampling *sampling;
+    int i, j, k;
 
     pOpener.set_file_name(INFILE);
     pReader = pOpener.open();
@@ -18,22 +31,23 @@ int main(int argc, char *argv[]) {
         printf("Could not open file\n");
         return 1;
     }
-    WAVESsampling *sampling;
     while(pReader->read_pulse()) {
         if(pReader->read_waves())
         {
-            for(int i = 0; i < pReader->waves->get_number_of_samplings(); i++) {
+            for(i = 0; i < pReader->waves->get_number_of_samplings(); i++) {
                 sampling = pReader->waves->get_sampling(i);
                 if(sampling->get_type() == PULSEWAVES_OUTGOING) {
                     printf("Outgoing Wave:");
                 } else {
-                    printf("Incoming Wave:");
+                    printf("Incoming Wave(s):");
                 }
                 printf("\n[");
-                for(int j = 0; j < sampling->get_number_of_segments(); j++ ) {
-                    for(int k = 0; k < sampling->get_number_of_samples(); k++) {
+                for(j = 0; j < sampling->get_number_of_segments(); j++ ) {
+                    printf(" [");
+                    for(k = 0; k < sampling->get_number_of_samples(); k++) {
                         printf("%u ", sampling->get_sample(k));
                     }
+                    printf("] ");
                 }
                 printf("]\n");
             }
